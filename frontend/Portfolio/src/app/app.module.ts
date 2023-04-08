2ff00af5240c3f9d50eaa603d7f3a79c67383071
Łukasz W <wojcikowski1@gmail.com>
2023-04-08 17:58:00 +0200

    simple pagination

--- frontend/Portfolio/src/app/app.module.ts
+++ frontend/Portfolio/src/app/app.module.ts
@@ -4,21 +4,36 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
+import { PortfolioComponent } from './portfolio-main/portfolio.component';
 import { PaginationComponent } from './pagination/pagination.component';
+import { PortfolioDetailsComponent } from './portfolio-details/portfolio-details.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/portfolio', pathMatch: 'full' },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'portfolio/:id', component: PortfolioDetailsComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     PortfolioComponent,
-    PaginationComponent
+    PaginationComponent,
+    PortfolioDetailsComponent,
+
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      routes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
+
+  
 })
 export class AppModule { }
--- frontend/Portfolio/src/app/portfolio-main/portfolio.component.ts
+++ frontend/Portfolio/src/app/portfolio-main/portfolio.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { PortfolioApiService } from '../service/portfolio-api.service';
 import { PageResponse } from '../responses/PageResponse';
 import { PortfolioEntry } from '../responses/PortfolioEntry';
+import { NavigationExtras } from '@angular/router';
+
 
 @Component({
   selector: 'app-portfolio',
@@ -13,17 +15,25 @@ export class PortfolioComponent implements OnInit {
   results: Array<PortfolioEntry> | undefined;
   page: number | undefined;
 
+
   constructor(private portfolioApi: PortfolioApiService) { }
 
   ngOnInit(): void {
-    this.portfolioApi.getEntries(1, 50, 'ASC', 'tittle').subscribe((data) => {
+    this.portfolioApi.getEntries(1, 3, 'ASC', 'tittle').subscribe((data) => {
       this.pageResponse= data;
       this.results = data.results
-      console.log(this.results);
+      console.log("total elements")
+      console.log(this.pageResponse.totalElements);
     });
     
   }
-  changePage(page: number): void{
+  onPageChanged(page: number): void{
     this.page=page
+    this.portfolioApi.getEntries(page, 3, 'ASC', 'tittle').subscribe((data) => {
+      this.pageResponse= data;
+      this.results = data.results
+      console.log("total elements")
+      console.log(this.pageResponse.totalElements);
+    });
   }
 }
